6b1a3b033bf51a7cce97b747ff937adcb2e15112
John VanDeWeghe <vandeweghe@lunixlabs.com>
2018-10-03 02:19:00 -0500

    add a more comprehensive lobby test

--- tests_integration/Api/Controller/LobbyControllerTest.php
+++ tests_integration/Api/Controller/LobbyControllerTest.php
@@ -5,15 +5,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LobbyControllerTest extends WebTestCase
 {
-    public function testGameCreation()
+    public function testJoiningGame()
     {
         $client = self::createClient();
 
         $client->request("POST", "/game");
-
         $gameData = json_decode($client->getResponse()->getContent(), true);
-
         $this->assertArrayIsValidGame($gameData);
+
+        $client->request("GET", "/game/{$gameData["id"]}");
+        $gameData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayIsValidGame($gameData);
+
+        $client->request("POST", "/player", [], [], ["CONTENT_TYPE" => "application/json"], json_encode(["gameId" => $gameData["id"]]));
+        $playerData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayIsValidPlayer($playerData);
+
+        $client->request("GET", "/player/{$playerData["id"]}");
+        $playerData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayIsValidPlayer($playerData);
     }
 
     private function assertArrayIsValidGame(array $gameData): void
@@ -28,5 +38,11 @@ class LobbyControllerTest extends WebTestCase
         $this->assertArrayHasKey(0, $gameData["map"]["tiles"] ?? []);
         $this->assertArrayHasKey("type", $gameData["map"]["tiles"][0] ?? []);
         $this->assertArrayHasKey("owner", $gameData["map"]["tiles"][0] ?? []);
+    }
+
+    private function assertArrayIsValidPlayer(array $playerData): void
+    {
+        $this->assertArrayHasKey("id", $playerData);
+        $this->assertArrayHasKey("playerNumber", $playerData);
     }
 }
