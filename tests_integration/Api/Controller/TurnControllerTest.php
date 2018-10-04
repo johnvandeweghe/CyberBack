58ad4142fa029b3d629893a20762b9026c20d646
John VanDeWeghe <vandeweghe@lunixlabs.com>
2018-10-03 22:58:37 -0500

    add first turn integration tests

--- tests_integration/Api/Controller/TurnControllerTest.php
+++ tests_integration/Api/Controller/TurnControllerTest.php
@@ -4,6 +4,7 @@ namespace App\IntegrationTests\Api\Controller;
 use App\Game\Manager;
 use App\Orm\Entity\Turn;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class TurnControllerTest extends WebTestCase
 {
@@ -20,39 +21,108 @@ class TurnControllerTest extends WebTestCase
         $client->request("GET", "/units/{$gameData["id"]}");
         $unitData = json_decode($client->getResponse()->getContent(), true);
 
-        $client->request("POST", "/turns", [], [], ["CONTENT_TYPE" => "application/json"], json_encode([
-            "gameId" => $gameData["id"],
-            "playerId" => $player1Data["id"]
-        ]));
-        $turn1Data = json_decode($client->getResponse()->getContent(), true);
-        $this->assertTurnIsValid($turn1Data);
+        $this->takeFirstTurn($client, $gameData, $player1Data, $unitData);
 
-//        $player1Tiles = array_filter($gameData["map"]["tiles"], function($tile) use ($player1Data) {
-//            return $tile["owner"] == $player1Data["playerNumber"];
-//        });
-        foreach($unitData as $index => $unit) {
-            if($unit["coordinates"] === null) {
-                $client->request("POST", "/unitAction", [], [], ["CONTENT_TYPE" => "application/json"], json_encode([
-                    "turnId" => $turn1Data["id"],
-                    "unitId" => $unit["id"],
-                    "type" => Manager::UNIT_ACTION_MOVE,
-                    "args" => ["path" => [["x" => 0, "y" => 0]]]
-                ]));
-                $moveResponse = json_decode($client->getResponse()->getContent(), true);
-                $this->assertUnitActionIsValid($moveResponse);
+        $this->takeFirstTurn($client, $gameData, $player2Data, $unitData);
+    }
+
+    private function getOwnedTilesWithCoordinates($tiles, $width, $playerNumber)
+    {
+        //Add coordinates, and filter to tiles that are owned by $playerNumber
+        return array_values(array_filter(
+            array_map(
+                function($tile, $index) use ($width) {
+                    return array_merge($tile, [
+                        "x" => $index % $width,
+                        "y" => floor($index / $width)
+                    ]);
+                },
+                $tiles,
+                array_keys($tiles)
+            ),
+            function($tile) use ($playerNumber) {
+                return $tile["owner"] == $playerNumber;
             }
-        }
+        ));
     }
 
     private function assertTurnIsValid($turnData)
     {
         $this->assertArrayHasKey("id", $turnData);
         $this->assertArrayHasKey("status", $turnData);
-        $this->assertEquals(Turn::STATUS_IN_PROGRESS, $turnData["status"]);
+        $this->assertContains($turnData["status"], [Turn::STATUS_IN_PROGRESS, Turn::STATUS_COMPLETED]);
     }
 
-    private function assertUnitActionIsValid($unitAction)
+    private function assertUnitMoveActionIsValid($unitAction)
     {
+        $this->assertArrayHasKey("unitId", $unitAction);
+        $this->assertArrayHasKey("turnId", $unitAction);
+        $this->assertArrayHasKey("type", $unitAction);
+        $this->assertEquals(Manager::UNIT_ACTION_MOVE, $unitAction["type"]);
+        $this->assertEquals("success", $unitAction["status"]);
+        $this->assertArrayHasKey("affectedUnitIds", $unitAction);
+        $this->assertCount(1, $unitAction["affectedUnitIds"]);
+    }
 
+    /**
+     * @param $gameData
+     * @param $playerData
+     * @param $unitData
+     * @param \Symfony\Bundle\FrameworkBundle\Client $client
+     * @param $turnData
+     */
+    private function placeAllUnitsForPlayer($gameData, $playerData, $unitData, $client, $turnData): void
+    {
+        $playerTiles = $this->getOwnedTilesWithCoordinates($gameData["map"]["tiles"], $gameData["map"]["width"],
+            $playerData["playerNumber"]);
+
+        $targetTileIndex = 0;
+
+        foreach ($unitData as $index => $unit) {
+            if ($unit["coordinates"] === null && $unit["owner"] === $playerData["playerNumber"]) {
+                $client->request("POST", "/unitAction", [], [], ["CONTENT_TYPE" => "application/json"], json_encode([
+                    "turnId" => $turnData["id"],
+                    "unitId" => $unit["id"],
+                    "type" => Manager::UNIT_ACTION_MOVE,
+                    "args" => [
+                        "path" => [
+                            [
+                                "x" => $playerTiles[$targetTileIndex]["x"],
+                                "y" => $playerTiles[$targetTileIndex]["y"]
+                            ]
+                        ]
+                    ]
+                ]));
+                $targetTileIndex++;
+                $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+                $moveResponse = json_decode($client->getResponse()->getContent(), true);
+                $this->assertUnitMoveActionIsValid($moveResponse);
+            }
+        }
+    }
+
+    /**
+     * @param \Symfony\Bundle\FrameworkBundle\Client $client
+     * @param $gameData
+     * @param $playerData
+     * @param $unitData
+     */
+    private function takeFirstTurn($client, $gameData, $playerData, $unitData): void
+    {
+        $client->request("POST", "/turns", [], [], ["CONTENT_TYPE" => "application/json"], json_encode([
+            "gameId" => $gameData["id"],
+            "playerId" => $playerData["id"]
+        ]));
+        $turnData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertTurnIsValid($turnData);
+
+        $this->placeAllUnitsForPlayer($gameData, $playerData, $unitData, $client, $turnData);
+
+        $client->request("PATCH", "/turns/{$turnData["id"]}", [], [], ["CONTENT_TYPE" => "application/json"],
+            json_encode([
+                "status" => Turn::STATUS_COMPLETED
+            ]));
+        $turnData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertTurnIsValid($turnData);
     }
 }
